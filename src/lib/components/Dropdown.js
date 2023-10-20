0d42062856a2ef4bac2717c863c3a66ebf89a499
Lisitius <benjamin.fichaux@gmail.com>
2023-10-20 02:18:04 +0200

    Complete change to add a search field in the dropdown

--- src/lib/components/Dropdown.js
+++ src/lib/components/Dropdown.js
@@ -1,8 +1,7 @@
-import React from "react";
+import React, { useState, useEffect, useRef } from "react";
 import "./Dropdown.css";
 
 const Dropdown = ({
-  type,
   name,
   value,
   options,
@@ -10,26 +9,75 @@ const Dropdown = ({
   defaultOption,
   formErrors,
 }) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [search, setSearch] = useState("");
+  const [filteredOptions, setFilteredOptions] = useState(options);
+  const wrapperRef = useRef(null);
+
+  useEffect(() => {
+    if (search.length === 0) {
+      setFilteredOptions(options);
+    } else {
+      const lowercasedSearch = search.toLowerCase();
+      const filtered = options.filter((option) =>
+        option.value.toLowerCase().includes(lowercasedSearch)
+      );
+      setFilteredOptions(filtered);
+    }
+  }, [search, options]);
+
+  useEffect(() => {
+    function handleClickOutside(event) {
+      if (wrapperRef.current && !wrapperRef.current.contains(event.target)) {
+        setIsOpen(false);
+      }
+    }
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [wrapperRef]);
+
+  const handleToggle = () => setIsOpen(!isOpen);
+
+  const handleSearchChange = (e) => {
+    setSearch(e.target.value);
+  };
+
+  const handleOptionClick = (value) => {
+    onChange({ target: { name, value } });
+    setIsOpen(false);
+    setSearch("");
+  };
+
   return (
-    <div>
-      <select
-        className={`select ${formErrors[name] ? "select-error" : ""}`}
-        type={type}
-        name={name}
-        value={value}
-        onChange={onChange}
-      >
-        {defaultOption && (
-          <option value="" disabled hidden>
-            {defaultOption}
-          </option>
-        )}
-        {options.map((option, index) => (
-          <option key={index} value={option.value}>
-            {option.value}
-          </option>
-        ))}
-      </select>
+    <div
+      ref={wrapperRef}
+      className={`dropdown ${formErrors[name] ? "dropdown-error" : ""}`}
+    >
+      <button className="dropdown-button" onClick={handleToggle}>
+        {value || defaultOption}
+      </button>
+      {isOpen && (
+        <div className="dropdown-list">
+          <input
+            type="text"
+            value={search}
+            onChange={handleSearchChange}
+            className="search-input"
+            placeholder="Recherche..."
+          />
+          {filteredOptions.map((option, index) => (
+            <div
+              key={index}
+              className="dropdown-list-item"
+              onClick={() => handleOptionClick(option.value)}
+            >
+              {option.label || option.value}
+            </div>
+          ))}
+        </div>
+      )}
       {formErrors[name] && <p className="error-text">{formErrors[name]}</p>}
     </div>
   );
